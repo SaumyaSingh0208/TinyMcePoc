da6901251ae29248feb174e3d478860eaea3a442
SaumyaSingh0208 <saumya0231@gmail.com>
2025-04-15 01:53:45 +0530

    Initial tinemce project

---
+++ tinymce-project/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TEditor from './Components/TEditor';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to TinyEditor</h1>
+      <TEditor/>
+    </div>
+  );
+}
+
+export default App;
